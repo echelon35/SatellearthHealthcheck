d67fcf27bafc47b5e770265e76a949234ddf076b
echelon35 <46542283+echelon35@users.noreply.github.com>
2024-09-12 21:35:20 +0200

    Update deploy.yml

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.SATELLEARTH_HEALTHCHECK_PORT || 3000;
 
+var allowedOrigins = ['http://localhost:4200'];
+
 // Liste des APIs à vérifier en interne
 const apis = [
   { name: 'SatellearthAPI', url: `${process.env.SATELLEARTH_API}/health` },
   { name: 'DisasterAPI', url: `${process.env.DISASTER_API}/health` },
-  { name: 'DisasterEater', url: `${process.env.DISASTER_EATER}/health` },
+//   { name: 'DisasterEater', url: `${process.env.DISASTER_EATER}/health` },
 ];
 
 // Liste des APIs à vérifier
@@ -18,11 +21,26 @@ const externalapis = [
     { name: 'GDACS', url: `https://www.gdacs.org/gdacsapi/api/events/geteventlist/MAP` },
   ];
 
+//Cors middleware
+app.use(cors({
+    origin: function(origin, callback){
+        // allow requests with no origin 
+        // (like mobile apps or curl requests)
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){
+                var msg = 'The CORS policy for this site does not ' +
+                            'allow access from the specified Origin.';
+                return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
+
 // Fonction pour vérifier la santé d'une API
 async function checkApiHealth(api) {
   try {
     const response = await axios.get(api.url, { timeout: 5000 });
-    return { name: api.name, url: api.url, status: 'OK', responseTime: response.duration };
+    return { name: api.name, url: api.url, status: (response.data ? 'OK' : 'NOK'), responseTime: response.duration };
   } catch (error) {
     return { name: api.name, url: api.url, status: 'ERROR', message: error.message };
   }
